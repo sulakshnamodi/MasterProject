4968dd8fa59daa9f2ee31c46c00238ac8950910c
Brijesh Modi <sulakshna.umrit1@gmail.com>
2025-12-02 16:04:21 +0100

    Preprocessing and Cleaning Pipeline
    Renames the column names in PIAAC dataset,
    Remove the columns with all missing values.

---
+++ eda/data_preprocessing.py
@@ -0,0 +1,63 @@
+# Data Preprocessing: 
+# Loads PIAAC data and modifies column name 
+# and saves the dataframe for reusing in future
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import os, sys
+
+# Norway data
+norway_data_root = r'G:\My Drive\Sulakshna\Sulakshna Drive\Codes\MasterProject\data\raw'
+output_folder = r'G:\My Drive\Sulakshna\Sulakshna Drive\Codes\MasterProject\data\preprocessed'
+norway_data_filename = 'prgnorp2.csv'
+norway_data_filepath = os.path.join(norway_data_root, norway_data_filename)
+
+# Codebook
+metadata_root = r'G:\My Drive\Sulakshna\Sulakshna Drive\Codes\MasterProject\data\metadata'
+codebook_filename = 'piaac-cy2-international-codebook.xlsx'
+codebook_filepath = os.path.join(metadata_root, codebook_filename)
+
+# Load the data, using the correct delimiter (;) and setting low_memory=False
+# This DataFrame 'nor_data_df' retains all special codes (e.g., '.', -9) as their literal values.
+norway_df = pd.read_csv(norway_data_filepath, sep=';', low_memory=False)
+codebook_df = pd.read_excel(codebook_filepath)
+
+print('Codebook Columns', codebook_df.columns)
+print('Norway Data Columns', norway_df.columns)
+
+# Rename column names in norway_df to readable values
+## 1. Create the Column Mapping Dictionary
+# We map the 'Variable' column (current names) to the 'Label' column (new names).
+
+# It's safest to ensure unique 'Variable' names before creating the dictionary.
+# If duplicates exist, this keeps the label from the *first* occurrence.
+column_mapping_series = codebook_df.drop_duplicates(subset=['Variable'], keep='first').set_index('Variable')['Label']
+column_mapping = column_mapping_series.to_dict()
+
+
+## 2. Rename the Columns
+# Use the .rename() method with the generated dictionary.
+# Setting axis=1 or columns= specifies that the mapping is for column names.
+# The rename operation will safely ignore any keys in the mapping that
+# don't correspond to a column in norway_df.
+norway_df = norway_df.rename(columns=column_mapping)
+
+
+# Remove all the variables which as . values everywhere 
+# Find columns where ALL values are the string '.'
+# 1. Use the equality operator (==) to check every cell.
+# 2. Use .all() to check if ALL values in the resulting boolean Series for that column are True.
+# 3. Use .index to get the list of column names that meet this criteria.
+cols_to_drop = norway_df.columns[
+    (norway_df == '.').all()
+].tolist()
+
+# 4. Drop the identified columns from the DataFrame
+norway_df = norway_df.drop(columns=cols_to_drop)
+
+
+# Save the data frame as csv
+output_filepath = os.path.join(output_folder, 'norway_data_cleaned.csv')
+norway_df.to_csv(output_filepath, index=False)
+
+
